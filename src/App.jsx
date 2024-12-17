a3c4528b2b4c9fb1c3c77e2a6578479ed4ee46be
mahla-sk <mahlaa.samanduk@gmail.com>
2024-12-18 00:20:34 +0100

    vandetails

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
-
+import "./server.jsx"
+import VanDetail from "./pages/VanDetail"
 
 function App() {
  
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/vans" element={<Vans/>}/>
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+import Vans from "./Vans"
+
+export default function VanDetail() {
+    const param=useParams()
+    const [van,setVan]=React.useState(null)
+
+    React.useEffect(()=>{
+        fetch(`/api/vans/${param.id}`)
+        .then(res=>res.json())
+        .then(data=>setVan(data.vans))
+    },[param.id])
+    return(
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
+} 
